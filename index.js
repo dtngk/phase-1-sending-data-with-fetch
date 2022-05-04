915a378bebc9f47b92e8f0740ff9d63e1e11baf6
Dat Nguyen <dnguyenk2@gmail.com>
2022-05-03 17:10:30 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(tname, email){
+ return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: tname,
+      email: email,
+    }),
+  })
+  .then(function(reponse){
+    return reponse.json();
+  })
+  .then(function (object){
+    const text = document.createElement("p");
+    text.innerHTML = object.id;
+    document.querySelector('body').appendChild(text);
+   console.log(object);
+  })
+  .catch(function (error){
+    const text = document.createElement("p");
+    text.innerHTML = error;
+    document.querySelector('body').appendChild(text);
+  });
+}
